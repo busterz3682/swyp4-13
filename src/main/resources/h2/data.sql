959a9750d9e5ea84a6bf4dcdc7d43f33e6f17a44
busterz3682 <ytn1574@gmail.com>
2024-05-14 16:57:52 +0900

    dd

---
+++ src/main/resources/h2/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO `user_card` (`picture`, `title`, `content`) VALUES ('http://example.com/picture.png', '묵언수행형', '말이 적은편');
+
+insert into `Userinfo` (`id`, `username`, `password`) values (1, 'test', '1');
